2a6b88cebae1a7c9d44f1eeb42e24be29ffb2e43
aurakei <aurakeith@gmail.com>
2023-01-12 13:57:16 +0300

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/Layout.module.css'
+
+export default function Layout() {
+  return (
+    <div className='{styles.container}'>
+        <main className='{styles.main}'></main>
+        
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    
+      <div>
+        <Head>
+          <title>Birds news</title>
+          <meta name='keywords' content='birds, birds species'/>
+        </Head>
+        <h1>welcome to the new next app</h1>
+      </div>
+    
+  )
+}
