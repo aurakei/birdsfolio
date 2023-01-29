be8a64207de656b9e0685d66d7fa79e0e89ee973
aurakei <aurakeith@gmail.com>
2023-01-29 13:44:50 +0300

    adding the about, home and bird category page

---
+++ pages/index.jsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    
+      <div>
+        <Head>
+          <title>Birds news</title>
+          <meta name='keywords' content='birds, birds species'/>
+        </Head>
+        <header>
+          <nav>
+            <img src="" alt="" />
+            <a href="/bird-category">categories</a>
+            <a href="/about">About Us</a>
+            <a href="/">Home</a>
+          </nav>
+        </header>
+        <h1>welcome to the new birds info app</h1>
+
+        <footer>
+
+        </footer>
+      </div>
+    
+  )
+}
